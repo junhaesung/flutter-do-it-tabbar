75faaf489a70b397643a43a3b051e11e19ddc970
Haeseong Jeon <seong0428@gmail.com>
2021-06-06 00:14:48 +0900

    2단계: SecondApp 클래스 추가

---
+++ lib/sub/secondPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(child: Center(child: Text('두 번째 페이지'))));
+  }
+}
